66fec855ba4ec76a141255a144fc4eca58b06c93
Artur <artur-fedyuk@mail.ru>
2018-03-15 01:33:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+    let N = 1;
+    for (let i = 0; i < pairs.length; i++) {
+        N *= Math.pow(pairs[i][0], pairs[i][1]);
+    }
+    if (N > 100000000)
+        return 0;
+
+    let bTemplate = s.split('');
+    let res = 0;
+    let counter = 0;
+
+    for (let k = 1; k <= N; k++) {
+        for (let j = 0; j < bTemplate.length; j++) {
+            if ((NOD([k + j, N]) === 1) && ( bTemplate[j] == 1)) {
+                counter++;
+            } else if (NOD([k + j, N]) > 1 && bTemplate[j] == 0) {
+                counter++;
+            }
+        }
+        if (counter === bTemplate.length) {
+            res++;
+            counter = 0;
+        } else {
+            counter = 0;
+        }
+    }
+
+    return res%1000000007;
+};
+function NOD(A) {
+    let n = A.length, x = Math.abs(A[0]);
+    for (let i = 1; i < n; i++) {
+        let y = Math.abs(A[i]);
+        while (x && y) {
+            (x > y) ? (x %= y) : (y %= x);
+        }
+        x += y;
+    }
+    return x;
 }
